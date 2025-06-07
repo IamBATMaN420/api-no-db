71f2be8bb24394c3a9b7a7d3d1cafe4d87ce0f91
helper.dev <basakayush942@gmail.com>
2025-06-07 11:36:09 +0530

    random id by crypto added

--- src/index.js
+++ src/index.js
@@ -2,53 +2,61 @@ import express from "express"
 import crypto from "crypto";
 
 const app = express();
-//hey working
+
 app.use(express.json())
 
 const products = [
   {
+    id: "0bf735e2-fb4a-445a-9dcd-7b672ce62993",
     name: "Laptop",
     price: 49999.99,
     quantity: 1,
     active: true
   },
   {
+    id: "0bf735e2-fb4a-445a-9dcd-7b672ce62994",
     name: "Wireless Mouse",
     price: 799.00,
     quantity: 2,
     active: true
   },
   {
+    id: "0bf735e2-fb4a-445a-9dcd-7b672ce62997",
     name: "Mechanical Keyboard",
     price: 2999.50,
     quantity: 1,
     active: false
   },
   {
+    id: "0bf735e2-fb4a-445a-9dcd-7b672ce62999",
     name: "27-inch Monitor",
     price: 13499.00,
     quantity: 1,
     active: true
   },
   {
+    id: "0bf735e2-fb4a-445a-9dcd-7b672ce62990",
     name: "USB-C Hub",
     price: 1499.00,
     quantity: 3,
     active: true
   },
   {
+    id: "0bf735e2-fb4a-445a-9dcd-7b672ce62995",
     name: "Noise Cancelling Headphones",
     price: 8999.99,
     quantity: 1,
     active: false
   },
   {
+    id: "0bf735e2-fb4a-445a-9dcd-7b672ce629988",
     name: "Webcam",
     price: 2199.00,
     quantity: 2,
     active: true
   },
   {
+    id: "0bf735e2-fb4a-445a-9dcd-7b672ce629977",
     name: "Gaming Chair",
     price: 12499.00,
     quantity: 1,
@@ -62,26 +70,29 @@ app.get("/api", (req, res) => {
 });
 
 app.get("/api/products", (req, res) => {
+
   res.json(products);
 })
 // reading all product
 
-app.post("/api/products", (req, res) => {
-  const { name, price, quantity, active } = req.body;
 
+app.post("/api/products", (req, res) => {
+
+  const { name, price, quantity, active } = req.body;
   if (!name) {
     res.status(422).json(
       { "message": "nahi honga bhai" }
     )
   }
-
   const id = crypto.randomUUID()
-  products.push({ id,name, price, quantity, active })
-  res.status(201).json({ "masage": "product is added" })
+  products.push({ id, name, price, quantity, active })
+  res.status(201).json({ "masage": "product is added", id })
+
+
 })
 // crateing product
 
-const id = crypto()
+
 
 app.get("/api/product/:id",)
 
